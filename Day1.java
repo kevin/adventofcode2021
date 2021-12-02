5deea7ddcf1b0c8d2d807816cb47d87f188a4e50
kevin <35074883+kevin@users.noreply.github.com>
2021-12-02 00:31:44 -0600

    upload day 1

---
+++ Day1.java
@@ -0,0 +1,51 @@
+// github.com/kevin
+// 12/2/2021
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Day1 {
+	
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		// part 1
+		
+		ArrayList<Integer> n = new ArrayList<>();
+		
+		int count = 0;
+		
+		n.add(in.nextInt());
+		
+		while (in.hasNextInt()) {
+			
+			n.add(in.nextInt());
+			if (n.get(n.size()-1) > n.get(n.size()-2)) {
+				count++;
+			}
+			
+		}
+		
+		System.out.println("Part 1: " + count);
+		
+		// part 2
+		
+		count = 0;
+		int numWindows = n.size()-2;
+		
+		int last = n.get(0) + n.get(1) + n.get(2); // last window calculated, start at the first window
+		
+		for (int i = 1; i < numWindows; i++) {
+			
+			int current = n.get(i) + n.get(i+1) + n.get(i+2); // current window
+			count += current > last ? 1 : 0;
+			last = current;
+			
+		}
+		
+		System.out.println("Part 2: " + count);
+		
+	}
+
+}
